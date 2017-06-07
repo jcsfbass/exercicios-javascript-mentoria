33b7cb20de38e5fb07273de163f7040f52bd1536
Carlos Souza <jcsfbass@gmail.com>
2017-06-07 16:23:43 -0300

    Adicionando botao para calcular os fatores primos

--- fatores-primos/index.js
+++ fatores-primos/index.js
@@ -1,9 +1,13 @@
 const campoValor = document.querySelector('#valor');
 const campoResultado = document.querySelector('output');
+const botaoCalcular = document.querySelector('button');
 
-campoValor.addEventListener('input', () => {
+const listener = () => {
   const valor = parseInt(campoValor.value);
   const resultado = fatoresPrimos(valor);
 
   campoResultado.textContent = resultado;
-});
+};
+
+campoValor.addEventListener('input', listener);
+botaoCalcular.addEventListener('click', listener);
